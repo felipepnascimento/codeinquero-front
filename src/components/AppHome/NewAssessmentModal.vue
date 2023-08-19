d74272fbcf7d0fe202d3feef7693a9c65ca181f5
Felipe Nascimento <felipeplnasc92@gmail.com>
2023-08-19 19:39:15 -0300

    list assessment

---
+++ src/components/AppHome/NewAssessmentModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    width="600px"
+    light
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        outlined
+        v-bind="attrs"
+        v-on="on"
+        >
+        <v-icon left>
+          mdi-plus
+        </v-icon>
+        Nova Prova
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Escolha o tema da sua prova</span>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="name"
+          :rules="nameRules"
+          label="Digite o tema que deseja"
+          required
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          @click=generateAssessment
+        >
+          Gerar Prova
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+
+export default {
+  name: 'NewAssessmentModal',
+  components: {
+  },
+  methods: {
+    generateAssessment () {
+      this.closeModal()
+    },
+    closeModal () {
+      this.dialog = false
+    }
+  },
+  data () {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app color="primary">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Study Bro
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <Assessments />
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary ">
+      <v-spacer></v-spacer>
+
+      <NewAssessmentModal />
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <!-- -->
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+import Assessments from '@/components/AppHome/Assessments'
+import NewAssessmentModal from '@/components/AppHome/NewAssessmentModal'
+
+export default {
+  name: 'AppHome',
+  components: {
+    Assessments,
+    NewAssessmentModal
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
