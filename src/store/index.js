89e4229bc6fa1284c4a0a1ba83911b336fd998f5
Felipe Nascimento <felipeplnasc92@gmail.com>
2023-08-20 02:28:41 -0300

    remove assessment

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import assessment from './modules/assessment'
 import session from './modules/session'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    assessment,
     session
   }
 })
