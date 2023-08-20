e422c9f901fa4e864b9b61ab386d4aaea2adeb7e
Mateus Pereira <mateuspereira97@icloud.com>
2023-08-20 02:25:02 -0300

    add finish questions flow

--- src/components/Questions/Questions.vue
+++ src/components/Questions/Questions.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="questions-wrapper" v-if="this.questions.length > 0">
+  <div class='assessment-report-wrapper' v-if="this.finished">
+    <div class="header">
+      <p class="text-h2">Prova finalizada!</p>
+      <p class="text-h4">Nota: {{this.grade}}/100</p>
+    </div>
+    <p class="text-h5">Já temos um plano de estudo pra você:</p>
+    <ul
+      v-for="(studyPlanItem, i) in this.studyPlans"
+      :key="i"
+    >
+      <li>
+        <p class="text-h5">Topico: {{studyPlanItem.topic}}</p>
+        <p class="text-h6">{{studyPlanItem.description}}</p>
+        <p class="text-h6">Onde encontrar: {{studyPlanItem.subject.whereFind}}</p>
+      </li>
+    </ul>
+  </div>
+  <div class="questions-wrapper" v-else-if="this.questions.length > 0">
     <div>
       <span class="text-h7">Pergunta {{ selectedQuestion.index + 1 }} de {{ this.questions.length }}</span>
     </div>
@@ -12,6 +29,14 @@
         <Question :question="question" :index="i + 1" :session-id="sessionId" />
       </v-expansion-panel>
     </v-expansion-panels>
+    <div class='submit-section'>
+      <v-btn
+          color="primary"
+          @click="finishSession()"
+      >
+        Finalizar prova
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -40,13 +65,24 @@ export default {
   methods: {
     setSelectedQuestion (question) {
       this.selectedQuestion = question
+    },
+    async finishSession () {
+      const { status, data } = await sessionApi.finish(this.sessionId)
+      if (status === 200) {
+        this.finished = true
+        this.grade = Math.ceil(data.grade)
+        this.studyPlans = data.studyPlans
+      }
     }
   },
   data () {
     return {
       selectedPanel: 0,
       questions: [],
-      selectedQuestion: null
+      selectedQuestion: null,
+      finished: false,
+      grade: null,
+      studyPlans: []
     }
   }
 }
@@ -58,4 +94,25 @@ export default {
   row-gap: 32px;
   padding: 0 48px;
 }
+
+.submit-section {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 16px;
+}
+
+.assessment-report-wrapper {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+}
+
 </style>
